53333884227a0ee19e53ea19986ea6d14a1886cb
Nate Mollica <nathanmollica@gmail.com>
2020-12-27 09:28:55 -0800

    Removed old directories

--- The_Basics/basics-sqrt.rb
+++
@@ -1,7 +0,0 @@
-# This code calculates the squares of a given input float number.
-
-puts "Input number you want to square: "
-  num = gets.chomp.to_i
-  square_root = Integer.sqrt(num)
-  puts "#{num}'s square root is #{square_root}."
-  
